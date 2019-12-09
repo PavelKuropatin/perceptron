f9dba3439d553149fef2bb3a32dffd1ffbba9e59
PavelKuropatin <pavelkuropatin@gmail.com>
2019-12-09 14:01:43 +0300

    add run mode

--- ui.py
+++ ui.py
@@ -9,32 +9,40 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
 from ml.ml_utils.data_utils import read_matrix_data, read_csv, write_matrix_data
 from ml.ml_utils.image_utils import to_bit_pixels
 from ml.perceptron import Perceptron
-from utils.constants import PWD, IMAGE, A_FILE, CLASSES_FILE, LAMBDAS_FILE
+from utils.constants import PWD, IMAGE, A_FILE, CLASSES_FILE, LAMBDAS_FILE, LAMBDAS_0_FILE
 
 
-class MainWindow(QMainWindow):
+class PerceptronApp(QMainWindow):
 
-    def __init__(self, file):
+    def __init__(self, file, mode):
         # self.image_table: QTableWidget = None
         self.__image_bitmap = None
         self.__perceptron: Perceptron = None
         self.__answers = []
         super().__init__()
         uic.loadUi(file, self)
-
-        self.__init_data()
-        self.learn_button.setEnabled(False)
+        if mode == "learn":
+            self.learn_button.setEnabled(True)
+            self.__init_data(A_FILE, LAMBDAS_0_FILE, CLASSES_FILE)
+            self.learn_perceptron()
+            write_matrix_data(self.__perceptron.lambdas, LAMBDAS_FILE)
+            sys.exit(0)
+        elif mode == "ui":
+            self.learn_button.setEnabled(False)
+            self.__init_data(A_FILE, LAMBDAS_FILE, CLASSES_FILE)
+            self.show()
+        else:
+            print(f"no valid mode: {mode}")
+            sys.exit(0)
         # self.learn_perceptron()
         # sys.exit(0)
-        self.show()
 
-    def __init_data(self):
-        a = read_matrix_data(A_FILE)
-        # lambdas = read_matrix_data(LAMBDAS_0_FILE)
-        lambdas = read_matrix_data(LAMBDAS_FILE)
+    def __init_data(self, a_file, lambdas_file, class_file):
+        a = read_matrix_data(a_file)
+        lambdas = read_matrix_data(lambdas_file)
         self.__perceptron = Perceptron(a, lambdas)
-        self.__classes = {int(index): key for key, index in read_csv(CLASSES_FILE)}
-        self.__classes_r = {key: int(index) for key, index in read_csv(CLASSES_FILE)}
+        self.__classes = {int(index): key for key, index in read_csv(class_file)}
+        self.__classes_r = {key: int(index) for key, index in read_csv(class_file)}
 
     def load_image(self):
         image = self.__open_file()
@@ -59,11 +67,11 @@ class MainWindow(QMainWindow):
                 for image in os.listdir(class_images_dir)
             ]
         _.random.shuffle(images_to_load)
+        i = 1
         for image, proper_class in images_to_load:
-            print(image, proper_class)
+            print(i, image, proper_class)
+            i += 1
             self.__perceptron.learning(to_bit_pixels(image), proper_class)
-
-        write_matrix_data(self.__perceptron.lambdas, LAMBDAS_FILE)
 
     def recognize_image(self):
         if self.__image_bitmap is not None:
@@ -78,6 +86,9 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == "__main__":
+    args = sys.argv[1:]
+    mode = args[0] if len(args) != 0 else "ui"
+    print(mode)
     app = QApplication(sys.argv)
-    ex = MainWindow("window.ui")
+    ex = PerceptronApp("window.ui", mode)
     sys.exit(app.exec_())
